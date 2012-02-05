0c53b11a565bbc3fa11d070a1966d37d55208a46
tzangms <tzangms@gmail.com>
2012-02-05 20:30:47 +0800

    modify wsgi.py for uwsgi deployment

--- pycon_project/deploy/wsgi.py
+++ pycon_project/deploy/wsgi.py
@@ -1,6 +1,7 @@
 from django.core.handlers.wsgi import WSGIHandler
-
 import pinax.env
+import sys
+import os
 
 
 # setup the environment for Django and Pinax
@@ -8,4 +9,5 @@ pinax.env.setup_environ(__file__)
 
 
 # set application for WSGI processing
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 application = WSGIHandler()
