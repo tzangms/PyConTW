2b7b711924cab9413660f3cfe9367a4bd1edde32
tzangms <tzangms@gmail.com>
2012-04-23 12:01:51 +0800

    speaker admin add has_photo display.

--- pycon_project/symposion/speakers/admin.py
+++ pycon_project/symposion/speakers/admin.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 
 from symposion.speakers.models import Speaker
 
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = ["name", "email", "twitter_username", "sessions_preference", 'has_photo', "created"]
+    search_fields = ["name"]
 
-admin.site.register(Speaker,
-    list_display = ["name", "email", "twitter_username", "sessions_preference", "created"],
-    search_fields = ["name"],
-)
+    def has_photo(self, obj):
+        return obj.photo != ''
+    has_photo.boolean = True
+
+admin.site.register(Speaker, SpeakerAdmin)
